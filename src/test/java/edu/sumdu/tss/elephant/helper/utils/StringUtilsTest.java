c5489c2e7ed11230e72dd04220b2498c012b4467
BlackVVell <86350779+BlackVVell@users.noreply.github.com>
2021-11-25 15:40:54 +0200

    100% StringUtils

---
+++ src/test/java/edu/sumdu/tss/elephant/helper/utils/StringUtilsTest.java
@@ -0,0 +1,33 @@
+package edu.sumdu.tss.elephant.helper.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    StringUtils stringUtils = new StringUtils();
+
+    @Test
+    void randomAlphaStringTest() {
+        String s = stringUtils.randomAlphaString(8);
+        assertEquals(8, s.length());
+    }
+
+    @Test
+    void uuid() {
+        assertNotNull(stringUtils.uuid());
+    }
+
+    @Test
+    void replaceLastTest() {
+        String s = stringUtils.replaceLast("52839", "28", " ");
+        assertEquals("5 39", s);
+    }
+
+    @Test
+    void notReplaceLastTest() {
+        String s = stringUtils.replaceLast("52839", "n", " ");
+        assertEquals("52839", s);
+    }
+}
