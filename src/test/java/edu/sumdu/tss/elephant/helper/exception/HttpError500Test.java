9a1bd10d19908262dc7e8ee07d6b4d1999d076d6
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2021-11-25 19:44:44 +0200

    HttpError500 100%

---
+++ src/test/java/edu/sumdu/tss/elephant/helper/exception/HttpError500Test.java
@@ -0,0 +1,40 @@
+package edu.sumdu.tss.elephant.helper.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpError500Test {
+
+    @Test
+    void HttpError500WithEx(){
+        NullPointerException exception = new NullPointerException();
+        HttpError500 httpError500 = new HttpError500(exception);
+        assertEquals(exception,httpError500.getCause());
+    }
+
+    @Test
+    void HttpError500WithMes(){
+        String equals = "edu.sumdu.tss.elephant.helper.exception.HttpError500: Error";
+        String massage = "Error";
+        HttpError500 httpError500 = new HttpError500(massage);
+        assertEquals(equals,httpError500.toString());
+    }
+
+    @Test
+    void HttpError500WithExAndMes(){
+        NullPointerException exception = new NullPointerException();
+        String equals = "edu.sumdu.tss.elephant.helper.exception.HttpError500: Error";
+        String message = "Error";
+        HttpError500 httpError500 = new HttpError500(message,exception);
+        assertEquals(equals,httpError500.toString());
+    }
+
+    @Test
+    void HttpError500(){
+        HttpError500 httpException = new HttpError500();
+        int code = 500;
+        assertEquals(httpException.getCode(),code);
+    }
+
+}
