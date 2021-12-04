3b2ab01084c6a0437a72f0339802e0ebbc6bf705
LLOCKED <vladmatvienko4@gmail.com>
2021-12-04 11:56:12 +0200

    100 % MessageBundle

---
+++ src/test/java/edu/sumdu/tss/elephant/helper/utils/MessageBundleTest.java
@@ -0,0 +1,37 @@
+package edu.sumdu.tss.elephant.helper.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBundleTest {
+
+
+    @Test
+    void getTest() {
+        MessageBundle mess = new MessageBundle("EN");
+        String str = mess.get("mail.conformation");
+        assertEquals("Elephant: Welcome to the club buddy", str);
+    }
+
+    @Test
+    void getErrorTest(){
+        MessageBundle mess = new MessageBundle("EN");
+        String str = mess.get("mail.conformati");
+        assertEquals("I18n not found:mail.conformati", str);
+    }
+
+    @Test
+    void constructorTest(){
+        MessageBundle mess = new MessageBundle("EN");
+        assertEquals("Elephant: Welcome to the club buddy",mess.get("mail.conformation"));
+
+    }
+
+    @Test
+    void testGet() {
+        MessageBundle mess = new MessageBundle("EN");
+        String str = mess.get("mail.conformation",1,2,3);
+        assertEquals("Elephant: Welcome to the club buddy", str);
+    }
+}
