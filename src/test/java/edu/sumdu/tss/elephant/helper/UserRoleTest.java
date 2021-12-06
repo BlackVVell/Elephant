c85abe81c00cad0fc1a7a8182223cb6b36d239bf
Nikol <scaliariy1@gmail.com>
2021-12-06 13:25:35 +0200

    100% UserRole

--- src/test/java/edu/sumdu/tss/elephant/helper/UserRoleTest.java
+++ src/test/java/edu/sumdu/tss/elephant/helper/UserRoleTest.java
@@ -1,6 +1,6 @@
 package edu.sumdu.tss.elephant.helper;
 
-import edu.sumdu.tss.elephant.helper.UserRole;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,30 +32,46 @@ class UserRoleTest {
 
     @Test
     void maxConnections() {
+        assertEquals(UserRole.ANYONE.maxConnections(), 0);
+        assertEquals(UserRole.UNCHEKED.maxConnections(), 0);
+        assertEquals(UserRole.BASIC_USER.maxConnections(), 5);
+        assertEquals(UserRole.PROMOTED_USER.maxConnections(), 5);
+        assertEquals(UserRole.ADMIN.maxConnections(), 5);
     }
 
     @Test
     void maxDB() {
+        assertEquals(UserRole.ANYONE.maxDB(), 0);
+        assertEquals(UserRole.UNCHEKED.maxDB(), 0);
+        assertEquals(UserRole.BASIC_USER.maxDB(), 2);
+        assertEquals(UserRole.PROMOTED_USER.maxDB(), 3);
+        assertEquals(UserRole.ADMIN.maxDB(), 100);
     }
 
     @Test
     void maxStorage() {
+        assertEquals(UserRole.ANYONE.maxStorage(), 0);
+        assertEquals(UserRole.UNCHEKED.maxStorage(), 0);
+        assertEquals(UserRole.BASIC_USER.maxStorage(), 20 * FileUtils.ONE_MB);
+        assertEquals(UserRole.PROMOTED_USER.maxStorage(), 50 * FileUtils.ONE_MB);
+        assertEquals(UserRole.ADMIN.maxStorage(), 50 * FileUtils.ONE_MB);
     }
 
     @Test
     void maxBackupsPerDB() {
+        assertEquals(UserRole.ANYONE.maxBackupsPerDB(), 0);
+        assertEquals(UserRole.UNCHEKED.maxBackupsPerDB(), 0);
+        assertEquals(UserRole.BASIC_USER.maxBackupsPerDB(), 1);
+        assertEquals(UserRole.PROMOTED_USER.maxBackupsPerDB(), 5);
+        assertEquals(UserRole.ADMIN.maxBackupsPerDB(), 10);
     }
 
     @Test
-    void getValue() {
-    }
-
-    @Test
-    void values() {
-    }
-
-    @Test
-    void valueOf() {
-
+    void maxScriptsPerDB() {
+        assertEquals(UserRole.ANYONE.maxScriptsPerDB(), 0);
+        assertEquals(UserRole.UNCHEKED.maxScriptsPerDB(), 0);
+        assertEquals(UserRole.BASIC_USER.maxScriptsPerDB(), 2);
+        assertEquals(UserRole.PROMOTED_USER.maxScriptsPerDB(), 5);
+        assertEquals(UserRole.ADMIN.maxScriptsPerDB(), 10);
     }
 }
