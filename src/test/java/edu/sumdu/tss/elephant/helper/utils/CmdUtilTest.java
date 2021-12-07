503a795ead441561a9b94c0fc1831e3128bf6945
LLOCKED <vladmatvienko4@gmail.com>
2021-12-07 18:03:10 +0200

    100% CmdUtil

--- src/test/java/edu/sumdu/tss/elephant/helper/utils/CmdUtilTest.java
+++ src/test/java/edu/sumdu/tss/elephant/helper/utils/CmdUtilTest.java
@@ -1,16 +1,39 @@
 package edu.sumdu.tss.elephant.helper.utils;
 
 import edu.sumdu.tss.elephant.helper.exception.BackupException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CmdUtilTest {
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
     @Test
     void execTest() {
-        String command = "ls";
+
+        String command = "clear";
         CmdUtil.exec(command);
+
+        assertEquals("Perform: clear", outputStreamCaptor.toString()
+                .trim());
     }
 
     @Test
